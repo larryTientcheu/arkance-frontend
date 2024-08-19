363f7a5366fa7da0f331d02548e5edc2177b259e
larry <larrytientcheu1@gmail.com>
2024-08-19 18:12:00 +0200

    fix: build

--- app/eleves/[id]/(components)/deleteButton.tsx
+++ app/eleves/[id]/(components)/deleteButton.tsx
@@ -2,15 +2,14 @@
 
 import { Button } from "@/components/ui/button";
 import { del } from "@/lib/api";
-import { useParams, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { FC } from "react";
 
-export const DeleteButton: FC = () => {
-  const params = useParams<{ id: string }>();
+export const DeleteButton: FC<{ id: string }> = ({ id }) => {
   const router = useRouter();
 
   const deleteStudent = async () => {
-    await del(`/Eleves/${params.id}`);
+    await del(`/Eleves/${id}`);
     router.push("/eleves")
     router.refresh()
   };
